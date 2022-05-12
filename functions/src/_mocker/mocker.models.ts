979883e798988306717e2bc0beb631a6d4fe96ca
Leonardo <lneninger@hotmail.com>
2022-05-12 09:32:47 -0400

    Update mocker.models.ts

--- functions/src/_mocker/mocker.models.ts
+++ functions/src/_mocker/mocker.models.ts
@@ -1,8 +1,16 @@
-export const mockedSignUp: IUserCreateForm = {
+export const mockedSignUp1: IUserCreateForm = {
   email: 'lneninger@hotmail.com',
   password: '123123',
   confirmPassword: '123123',
   displayName: 'Leonardo',
+  phoneNumber: '+17864553456',
+};
+
+export const mockedSignUp2: IUserCreateForm = {
+  email: 'lneninger@hipalanet.com',
+  password: '123123',
+  confirmPassword: '123123',
+  displayName: 'Leonardo Hipalanet',
   phoneNumber: '+17864553456',
 };
 
